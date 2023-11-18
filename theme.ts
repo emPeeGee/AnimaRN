0a2488fec2720330babe9f6d0b3ab16b254827f4
emPeeGee <eempeegee@gmail.com>
2023-11-18 10:28:26 +0200

    feat:  👩‍💻 Make the list

---
+++ theme.ts
@@ -0,0 +1,9 @@
+export const backgroundColorPlain = '#ffffff';
+export const backgroundColorGrouped = '#f2f2f7';
+export const fontColor = '#000000';
+export const separatorColor = '#dcdcdc';
+
+// export const backgroundColorPlain = '#3a3a3c';
+// export const backgroundColorGrouped = '#1c1c1e';
+// export const fontColor = '#ffffff';
+// export const separatorColor = '#4e4e4e';
